aeac470db08f91b127efd3f84c6ddab0d52e79c5
salmiyanti <psmsalmiyanti@gmail.com>
2017-03-05 16:36:30 +0800

    Sunday 05-Mar-17  4:36:29 PM

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -4,7 +4,7 @@ import { NavParams } from 'ionic-angular';
 
 import { AboutPage } from '../about/about';
 import { MapPage } from '../map/map';
-import { SchedulePage } from '../schedule/schedule';
+import { LoginPage } from '../login/login';
 import { SpeakerListPage } from '../speaker-list/speaker-list';
 
 
@@ -13,7 +13,7 @@ import { SpeakerListPage } from '../speaker-list/speaker-list';
 })
 export class TabsPage {
   // set the root pages for each tab
-  tab1Root: any = SchedulePage;
+  tab1Root: any = LoginPage;
   tab2Root: any = SpeakerListPage;
   tab3Root: any = MapPage;
   tab4Root: any = AboutPage;
